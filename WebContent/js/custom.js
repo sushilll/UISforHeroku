119ee451397ca81a28fd2fadedc2690d26cbd1b4
sushilll <sushil101094@gmail.com>
2017-03-29 17:45:01 +0530

    init

---
+++ WebContent/js/custom.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Sushil on 26-02-2017.
+ */
+ //scroll to about div
+$("#about").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".middle").offset().top}, 'slow');
+});
